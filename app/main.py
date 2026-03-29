8f92249b026eb588e57edce825afeaa2b5a87e75
DenilsonDonr <denilsontrillo@gmail.com>
2026-03-29 07:16:46 -0600

    add analysis router inclusion in main FastAPI app

--- app/main.py
+++ app/main.py
@@ -1,12 +1,18 @@
 # import fastAPI
 from fastapi import FastAPI
 
+# import the analysis router
+from app.routers import analysis
+
 # create an instance of the FastAPI class
 app = FastAPI(
     title="Text Analyzer API",
     description="Microservice API for analyzing text",
     version="1.0.0"
 )
+
+# include the analysis router in the main app
+app.include_router(analysis.router)
 
 # create route health, verify if the API is working
 @app.get("/health")
